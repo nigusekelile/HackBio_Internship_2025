27cc18cc98459ab20b166fe99c8c0614f5d0463c
Niguse Kelile <Niguse Kelile Lema>
2025-02-12 05:30:38 +0300

    stage zero final update

---
+++ HackBio_Stage_0_task_1.R
@@ -0,0 +1,23 @@
+getwd()
+setwd("/home/nigus/HackBio/HackBio_Internship_2025")
+getwd()
+
+
+Stage0_Task1 <- data.frame(
+  Name = c("Niguse Kelile Lema"),
+  Slack_Username = c("Niguse"),
+  Email = c("nigusmov16@gmail.com"),
+  Hobby = c("Reading"),
+  Country = c("Ethiopia"),
+  Discipline = c("Bioinformatics"),
+  programming_language = c("Python and R")
+)
+
+print(Stage0_Task1)
+
+Stage0_Task1$Name
+
+View(Stage0_Task1)
+
+
+print("The final result for HackBio Stage0 task one")
